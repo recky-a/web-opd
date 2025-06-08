dfc73238d3429a3db559f4c462b036d516016ec4
Recky <reckyanderson@gmail.com>
2025-06-09 00:01:04 +0700

    feat(nextjs): enable experimental React Compiler
    Enable the experimental React Compiler, to leverage potential performance optimizations.

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig: import('next').NextConfig = {
   output: 'standalone',
+  experimental: {
+    reactCompiler: true,
+  },
 };
 
 export default nextConfig;
